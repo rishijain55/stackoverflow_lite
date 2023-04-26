03b507b28abc5af60b366b2267a7bbca35a75d65
rishijain55 <82753708+rishijain55@users.noreply.github.com>
2023-04-26 23:52:29 +0530

    Add files via upload

---
+++ templates/temp_stuff/JS/login.js
@@ -0,0 +1,53 @@
+const loginForm = document.getElementById('login-form');
+
+if(loginForm != null)
+{  
+  loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const usernameInput = document.getElementById('login-form').elements.namedItem('username');
+    const usernameValue = usernameInput.value;
+    
+    console.log(usernameValue);
+
+    const usernamePassword = document.getElementById('login-form').elements.namedItem('password');
+    const passwordValue = usernamePassword.value;
+
+    console.log(passwordValue);
+    
+    // You can now use the usernameValue variable to do something with the
+  })
+}
+
+const signupForm = document.getElementById('signup-form');
+
+if(signupForm != null)
+{
+  signupForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const emailInput = document.getElementById('signup-form').elements.namedItem('email');
+  const emailValue = emailInput.value;
+
+  console.log(emailValue);
+
+  const usernameInput = document.getElementById('signup-form').elements.namedItem('username');
+  const usernameValue = usernameInput.value;
+
+  console.log(usernameValue);
+
+  const usernamePassword1 = document.getElementById('signup-form').elements.namedItem('password1');
+  const passwordValue1 = usernamePassword1.value;
+
+  const usernamePassword2 = document.getElementById('signup-form').elements.namedItem('password2');
+  const passwordValue2 = usernamePassword2.value;
+
+  console.log(passwordValue1);
+  console.log(passwordValue2);
+
+  if(passwordValue1 != passwordValue2){
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = 'Password do not match';
+  }
+  })
+}
